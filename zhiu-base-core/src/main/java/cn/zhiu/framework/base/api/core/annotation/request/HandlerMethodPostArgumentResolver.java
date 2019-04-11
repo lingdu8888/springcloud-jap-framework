ff56aca972032b3a35f049063be3a59161a1fb87
xyzhuzhou <xyzhuzhou@163.com>
2019-04-12 01:38:42 +0800

    添加RequestPostParam注解

---
+++ zhiu-base-core/src/main/java/cn/zhiu/framework/base/api/core/annotation/request/HandlerMethodPostArgumentResolver.java
@@ -0,0 +1,66 @@
+package cn.zhiu.framework.base.api.core.annotation.request;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.MethodParameter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Objects;
+
+public class HandlerMethodPostArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.hasParameterAnnotation(RequestPostParam.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+
+        RequestPostParam parameterAnnotation = methodParameter.getParameterAnnotation(RequestPostParam.class);
+
+        String name = parameterAnnotation.name();
+
+        JSONObject requestBody = getRequestBody(nativeWebRequest);
+
+        if (StringUtils.isEmpty(name)) {
+            name = methodParameter.getParameterName();
+        }
+
+
+        Type parameterType = methodParameter.getParameterType();
+
+        Object val = requestBody.getObject(name, parameterType);
+
+        if (Objects.isNull(val) && parameterAnnotation.required()) {
+            throw new IllegalArgumentException(String.format("required param %s is not present", name));
+        }
+
+        return val;
+
+    }
+
+    private JSONObject getRequestBody(NativeWebRequest webRequest) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        String jsonBody = (String) servletRequest.getAttribute(JSON_REQUEST_BODY);
+        if (jsonBody == null) {
+            try {
+                jsonBody = IOUtils.toString(servletRequest.getInputStream());
+                servletRequest.setAttribute(JSON_REQUEST_BODY, jsonBody);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return JSON.parseObject(jsonBody);
+
+    }
+
+    public static final String JSON_REQUEST_BODY = "JSON_REQUEST_BODY";
+}
---
+++ zhiu-base-core/src/main/java/cn/zhiu/framework/base/api/core/annotation/request/RequestPostParam.java
@@ -0,0 +1,61 @@
+package cn.zhiu.framework.base.api.core.annotation.request;
+
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * 映射POST请求体中JSON项第一层级的键值对
+ * 对POST请求体中
+ *
+ * @author zhuzz
+ * @time 2019 /04/12 00:35:17
+ */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RequestPostParam {
+
+
+    /**
+     * 是否必须
+     *
+     * @return the boolean
+     *
+     * @author zhuzz
+     * @time 2019 /04/12 00:42:03
+     */
+    boolean required() default true;
+
+    /**
+     * Value string.
+     *
+     * @return the string
+     *
+     * @author zhuzz
+     * @time 2019 /04/12 00:42:03
+     */
+    @AliasFor("name")
+    String value() default "";
+
+    /**
+     * Name string.
+     *
+     * @return the string
+     *
+     * @author zhuzz
+     * @time 2019 /04/12 00:42:03
+     */
+    @AliasFor("value")
+    String name() default "";
+
+    /**
+     * Default value string.
+     *
+     * @return the string
+     *
+     * @author zhuzz
+     * @time 2019 /04/12 00:42:03
+     */
+    String defaultValue() default "";
+}
